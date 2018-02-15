0957fc7c341c23baf2941cb34756b21a883da7f4
vagrant <vagrant@vagrant.vm>
2018-02-15 22:49:27 +0000

    Reformated code according to pep8 style guide

--- news.py
+++ news.py
@@ -2,9 +2,11 @@ import psycopg2
 
 
 def get_most_popular_three_article():
-    query = "select articles.title, result.num from (select replace(path,'/article/', '') as path_slug , count(*) " \
-            "as num from log group by path having path like '/article/%')as result,articles " \
-            "where result.path_slug = articles.slug order by result.num desc limit 3;"
+    query = "select articles.title, result.num from (select " \
+            "replace(path,'/article/', '') as path_slug , count(*) " \
+            "as num from log group by path having path like '/article/%')" \
+            "as result,articles where result.path_slug = articles.slug " \
+            "order by result.num desc limit 3;"
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute(query)
@@ -16,9 +18,12 @@ def get_most_popular_three_article():
 
 
 def get_most_popular_authors():
-    query = "select authors.name, sum(result.num)as total_views from (select replace(path,'/article/', '')" \
-            " as path_slug, count(*) as num from log group by path having path like '/article/%')as result," \
-            " articles, authors where result.path_slug = articles.slug and articles.author=authors.id " \
+    query = "select authors.name, sum(result.num)as total_views from" \
+            " (select replace(path,'/article/', '')" \
+            " as path_slug, count(*) as num from log group by path having path" \
+            " like '/article/%')as result," \
+            " articles, authors where result.path_slug = articles.slug " \
+            "and articles.author=authors.id " \
             "group by authors.name order by total_views desc;"
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
@@ -31,10 +36,14 @@ def get_most_popular_authors():
 
 
 def get_error_rate_of_day():
-    query = "select error.dateofday::timestamp::date as tata, (sum(error.total_error)/sum( total.total_request))as " \
-            "error_rate from (select time::timestamp::date  as dateofday, count(*) as total_error from log " \
-            "where log.status like '5%' or log.status like '4%' group by dateofday) as error," \
-            "(select time::timestamp::date as dateofday, count(*) as total_request from log group by dateofday)as total" \
+    query = "select error.dateofday::timestamp::date as tata," \
+            " (sum(error.total_error)/sum( total.total_request))as " \
+            "error_rate from (select time::timestamp::date  as dateofday, " \
+            "count(*) as total_error from log " \
+            "where log.status like '5%' or log.status like '4%' " \
+            "group by dateofday) as error," \
+            "(select time::timestamp::date as dateofday, " \
+            "count(*) as total_request from log group by dateofday)as total" \
             " where error.dateofday=total.dateofday group by tata ;"
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
